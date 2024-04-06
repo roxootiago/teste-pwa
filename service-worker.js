e9e1a091a63d2f5aa3f293c540877dadf733114a
roxootiago <tiago.roxo@hotmail.com>
2024-04-06 01:33:00 -0300

    add

--- service-worker.js
+++ service-worker.js
@@ -6,7 +6,7 @@ self.addEventListener('install', event => {
 
   event.waitUntil(
     caches.open(cacheName)
-      .then(cache => cache.addAll([
+      .then(cache => cache.add([
 
         './offline.html',
 
